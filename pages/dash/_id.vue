4fa58bbddd43598ce070f147c6f9b79971cf1c31
i860536 <tom.grek@sap.com>
2017-06-26 08:53:47 -0700

    Replace resize with JS version. Fix bugs

--- pages/dash/_id.vue
+++ pages/dash/_id.vue
@@ -37,63 +37,63 @@ export default {
                     let margin = {top: 0, right: 0, bottom: 0, left: 0};
                     // let svg = d3.select(document.querySelector("#tom")).append("svg")
 
-                    setInterval(() => {
-                      document.querySelector('#tom').innerHTML = '';
-
-                      let svg = d3.select(document.querySelector("#tom")).append("svg")
-
-                      let height = document.querySelector("#tom").parentElement.getBoundingClientRect().height - 16;
-                      let width = document.querySelector("#tom").parentElement.getBoundingClientRect().width - 16;
-                      svg.attr("height", height);
-                      svg.attr("width", width);
-                      var x = d3.scaleTime().range([0, width]);
-
-                      var y = d3.scaleLinear().range([0, height]);
-                      let g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-                      var parseTime = d3.timeParse("%d-%b-%y");
-                      var line = d3.line()
-                      .x(function(d) { return x(d.date); })
-                      .y(function(d) { return y(d.close); });
-
-                      d3.tsv("/data.tsv", function(d) {
-                        d.date = parseTime(d.date);
-                        d.close = +d.close;
-                        return d;
-                      }, function(error, data) {
-                        if (error) throw error;
-
-                        // x.domain(d3.extent(data, function(d) { return d.date; }));
-                        // y.domain(d3.extent(data, function(d) { return d.close; }));
-                        x.domain(d3.extent(data, function(d) { return d.date; }));
-                        y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-                        // g.append("g")
-                        //     .attr("transform", "translate(0," + 80 + ")")
-                        //     .call(d3.axisBottom(x))
-                        //   .select(".domain")
-                        //     .remove();
-                        //
-                        // g.append("g")
-                        //     .call(d3.axisLeft(y))
-                        //   .append("text")
-                        //     .attr("fill", "#000")
-                        //     .attr("transform", "rotate(-90)")
-                        //     .attr("y", 6)
-                        //     .attr("dy", "0.71em")
-                        //     .attr("text-anchor", "end")
-                        //     .text("Price ($)");
-
-                        g.append("path")
-                            .datum(data)
-                            .attr("fill", "none")
-                            .attr("stroke", "steelblue")
-                            .attr("stroke-linejoin", "round")
-                            .attr("stroke-linecap", "round")
-                            .attr("stroke-width", 1.5)
-                            .attr("d", line);
-                      });
-
-                    }, 1000);
+                    // setInterval(() => {
+                    //   document.querySelector('#tom').innerHTML = '';
+                    //
+                    //   let svg = d3.select(document.querySelector("#tom")).append("svg")
+                    //
+                    //   let height = document.querySelector("#tom").parentElement.getBoundingClientRect().height - 16;
+                    //   let width = document.querySelector("#tom").parentElement.getBoundingClientRect().width - 16;
+                    //   svg.attr("height", height);
+                    //   svg.attr("width", width);
+                    //   var x = d3.scaleTime().range([0, width]);
+                    //
+                    //   var y = d3.scaleLinear().range([0, height]);
+                    //   let g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                    //   var parseTime = d3.timeParse("%d-%b-%y");
+                    //   var line = d3.line()
+                    //   .x(function(d) { return x(d.date); })
+                    //   .y(function(d) { return y(d.close); });
+                    //
+                    //   d3.tsv("/data.tsv", function(d) {
+                    //     d.date = parseTime(d.date);
+                    //     d.close = +d.close;
+                    //     return d;
+                    //   }, function(error, data) {
+                    //     if (error) throw error;
+                    //
+                    //     // x.domain(d3.extent(data, function(d) { return d.date; }));
+                    //     // y.domain(d3.extent(data, function(d) { return d.close; }));
+                    //     x.domain(d3.extent(data, function(d) { return d.date; }));
+                    //     y.domain([0, d3.max(data, function(d) { return d.close; })]);
+                    //
+                    //     // g.append("g")
+                    //     //     .attr("transform", "translate(0," + 80 + ")")
+                    //     //     .call(d3.axisBottom(x))
+                    //     //   .select(".domain")
+                    //     //     .remove();
+                    //     //
+                    //     // g.append("g")
+                    //     //     .call(d3.axisLeft(y))
+                    //     //   .append("text")
+                    //     //     .attr("fill", "#000")
+                    //     //     .attr("transform", "rotate(-90)")
+                    //     //     .attr("y", 6)
+                    //     //     .attr("dy", "0.71em")
+                    //     //     .attr("text-anchor", "end")
+                    //     //     .text("Price ($)");
+                    //
+                    //     g.append("path")
+                    //         .datum(data)
+                    //         .attr("fill", "none")
+                    //         .attr("stroke", "steelblue")
+                    //         .attr("stroke-linejoin", "round")
+                    //         .attr("stroke-linecap", "round")
+                    //         .attr("stroke-width", 1.5)
+                    //         .attr("d", line);
+                    //   });
+                    //
+                    // }, 1000);
 
 
                     `,
@@ -115,26 +115,68 @@ export default {
       e.preventDefault();
       e.stopPropagation();
     },
+    wrap(content) {
+      return `<div style="height: 1rem; background-color: green;">Component Title${content}</div>`;
+    },
     dropped(e, id) {
       e.preventDefault();
       e.stopPropagation();
       let c = d3.select('.canvas-container');
 
-      this.components.push(c.append('div')
+      let div = c.append('div')
         .html(this.predefinedComponents[id].content)
         .attr('class', 'box')
         .attr('offsetX', e.pageX - this.svgOffsetX)
         .attr('offsetY', e.offsetY - 80 - this.svgOffsetY)
-        .style('resize', 'both')
-        .style('position', 'relative')
-        .style('overflow', 'auto')
-        .style('transform', `translate(${e.pageX}px, ${e.offsetY - 80}px)`)
+        .style('position', 'absolute')
+        .style('z-index', 40)
+        .style('transform', `translate(${e.pageX - 15}px, ${e.offsetY - 80}px)`)
           .call(d3.drag()
             .on('drag', this.dragged)
             .on('end', () => {
               //dragging = false;
             })
-          ));
+          );
+
+      div.append('div').style('height', '1.5rem').style('background-color', 'green').style('z-index', 50);
+
+          var resizer = document.createElement('div');
+          resizer.className = 'resizer';
+          resizer.style.width = '10px';
+          resizer.style.height = '10px';
+          resizer.style.background = 'red';
+          resizer.style.position = 'absolute';
+          resizer.style.visibility = 'hidden';
+          resizer.style.right = 0;
+          resizer.style.bottom = 0;
+          resizer.style.cursor = 'se-resize';
+          let node = div.node();
+          node.appendChild(resizer);
+          function Resize(e) {
+            if (!e.shiftKey) { return stopResize(e); }
+            //if (e.target !== div.node()) return;
+            e.preventDefault();
+            e.stopPropagation();
+             node.style.width = (e.clientX - parseInt(node.getAttribute('offsetX'))) + 'px';
+             node.style.height = (e.clientY - parseInt(node.getAttribute('offsetY')) - 40) + 'px';
+          }
+
+          function initResize(e) {
+            if (!e.shiftKey) return;
+            e.preventDefault();
+            e.stopPropagation();
+             document.addEventListener('mousemove', Resize, false);
+             document.addEventListener('mouseup', stopResize, false);
+          }
+          function stopResize(e) {
+              document.removeEventListener('mousemove', Resize, false);
+              document.removeEventListener('mouseup', stopResize, false);
+          }
+
+
+          resizer.addEventListener('mousedown', initResize, false);
+
+      this.components.push(div);
       eval(this.predefinedComponents[id].script);
     },
   },
@@ -153,6 +195,21 @@ export default {
   mounted() {
     var self = this;
     window.d3 = d3;
+
+    window.onkeydown = e => {
+      function stopShowingResizer(e) {
+        for (var node of document.getElementsByClassName('resizer')) {
+          node.style.visibility = 'hidden';
+        }
+        window.removeEventListener('keyup', stopShowingResizer, false);
+      };
+      if (e.key === 'Shift') {
+        window.addEventListener('keyup', stopShowingResizer, false);
+        for (var node of document.getElementsByClassName('resizer')) {
+          node.style.visibility = 'visible';
+        }
+      }
+    };
     const margin = {top: -5, right: -5, bottom: -5, left: -5};
     let bbox = document.getElementsByClassName('canvas-container')[0].getBoundingClientRect();
     const width = parseInt(bbox.width) - 4;
@@ -200,7 +257,6 @@ export default {
     let c_node = c.node();
 
     function dragged(d) {
-
       if (d3.event.sourceEvent.shiftKey) return;
       d3.event.sourceEvent.stopPropagation();
 
@@ -220,7 +276,6 @@ export default {
         styleStr = styleStr + ' scale(' + parseFloat(st[1]) + ')';
       }
       let meD3 = d3.select(this);
-
       meD3.style('transform', styleStr);
       meD3.attr('offsetX', (x - self.svgOffsetX - 40));
       meD3.attr('offsetY', (y - self.svgOffsetY - 40));
